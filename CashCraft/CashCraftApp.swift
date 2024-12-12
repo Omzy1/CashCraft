c129bd1bf00146dae1f0022bf1a8101f9a51449c
Omzy1 <omarsillah93@gmail.com>
2024-12-12 18:53:58 +0000

    Initial Commit

---
+++ CashCraft/CashCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashCraftApp.swift
+//  CashCraft
+//
+//  Created by omar sillah on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CashCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CashCraft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CashCraft
+//
+//  Created by omar sillah on 12/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
