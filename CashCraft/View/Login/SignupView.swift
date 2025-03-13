860367c31752d394f099e8e69eb2a6506b0d7f42
Omzy1 <omarsillah93@gmail.com>
2025-03-13 18:51:35 +0000

    adding sign up validation when user create an account

--- CashCraft/View/Login/SignupView.swift
+++ CashCraft/View/Login/SignupView.swift
@@ -10,50 +10,94 @@ import SwiftUI
 struct SignupView: View {
     @Environment(\.dismiss) var dismiss
     @StateObject private var viewmodel = SignUpViewModel()
+    // show the validation password text only after the user enter a word
+    @State private var showText: Bool = false
     var body: some View {
         ZStack {
             Color.white
                 .ignoresSafeArea()
             VStack {
-                Image(NSLocalizedString("signup_image", comment: "Sign up Image"))
-                    .resizable()
-                    .scaledToFit()
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(NSLocalizedString("signup_title", comment: " Sign up text"))
-                        .padding(.leading)
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .padding(.leading)
-                    InputView(text: $viewmodel.email, imageName: "at", textfield: "Email ID")
-                    InputView(text: $viewmodel.password, imageName: "lock", textfield: "Password")
+                Section {
+                    Image(NSLocalizedString("signup_image", comment: "Sign up Image"))
+                        .resizable()
+                        .scaledToFit()
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(NSLocalizedString("signup_title", comment: " Sign up text"))
+                            .padding(.leading)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .padding(.leading)
+                        if let emailError = viewmodel.emailError {
+                            Text(emailError)
+                                .foregroundColor(.red)
+                                .bold()
+                                .font(.caption)
+                                .padding(.horizontal)
+                                .padding(.horizontal)
+                        }
+                        //  email field
+                        InputView(text: $viewmodel.email, imageName: "at", textfield: "Email ID")
+                            .onChange(of: viewmodel.email) {
+                                // validate email while user typing
+                                viewmodel.validateEmail()
+                            }
+                        // password validation error
+                        if let passwordError = viewmodel.passwordError {
+                            if showText {
+                                Text(passwordError)
+                                    .foregroundColor(.red)
+                                    .bold()
+                                    .font(.caption)
+                                    .multilineTextAlignment(.leading)
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.horizontal)
+                                    .padding(.horizontal)
+                            }
+                            
+                        }
+                        // password field
+                        InputView(text: $viewmodel.password, imageName: "lock", textfield: "Password")
+                            .onChange(of: viewmodel.password) {
+                                viewmodel.validatePassword()
+                                showText = true
+                            }
+                    }
                 }
-                // login button
-                ButtonAuthentication(text: NSLocalizedString("signup_title", comment: "button signup title"), buttonAction: {
-                    viewmodel.signUp()
-                    print("Signup button tapped")
-                })
-                // line
-                LineOrSeperator()
-                // Google button login
-                ButtonGoogle(text: NSLocalizedString("google_title_button_signup", comment: " google title button login"), buttonAction: {
-                    print("Google button tapped")
-                })
-                
-                // Sign up
-                HStack {
-                    Text(NSLocalizedString("already_account", comment: "already have an account?"))
-                        .foregroundColor(.black.opacity(0.5))
-                    Button(action: {
-                        self.dismiss()
-                    }, label: {
-                        Text(NSLocalizedString("login_title", comment: " button login text"))
+                Section {
+                    // login button
+                    ButtonAuthentication(text: NSLocalizedString("signup_title", comment: "button signup title"), buttonAction: {
+                        viewmodel.signUp()
+                        print("Signup button tapped")
                     })
-                    .padding(.horizontal)
                 }
-                // hiding back button
-                .navigationBarBackButtonHidden(true)
-                .transition(.slide)
-                Spacer()
+                .disabled(!viewmodel.isFormValid)
+                Section {
+                    // line
+                    LineOrSeperator()
+                    // Google button login
+                    ButtonGoogle(text: NSLocalizedString("google_title_button_signup", comment: " google title button login"), buttonAction: {
+                        print("Google button tapped")
+                    })
+                }
+                
+                Section {
+                    // Sign up
+                    HStack {
+                        Text(NSLocalizedString("already_account", comment: "already have an account?"))
+                            .foregroundColor(.black.opacity(0.5))
+                        Button(action: {
+                            self.dismiss()
+                        }, label: {
+                            Text(NSLocalizedString("login_title", comment: " button login text"))
+                        })
+                        .padding(.horizontal)
+                    }
+                    // hiding back button
+                    .navigationBarBackButtonHidden(true)
+                    .transition(.slide)
+                    Spacer()
+                }
             }
         }
     }
--- CashCraft/ViewModels/SignUpViewModel.swift
+++ CashCraft/ViewModels/SignUpViewModel.swift
@@ -9,16 +9,64 @@ import Foundation
 
 @MainActor
 final class SignUpViewModel: ObservableObject {
-    
+    // email and password the user enter in the field
     @Published var email: String = ""
     @Published var password: String = ""
     
-    func signUp() {
-        guard !email.isEmpty , !password.isEmpty else {
-            // temporary changes, need proper validation
-            print("Please enter a valid email or password")
-            return
+    // validation email and password error string
+    @Published var emailError: String? = ""
+    @Published var passwordError: String? = ""
+    
+    struct PasswordRequirments {
+        var hasMinLength: Bool
+        var hasUpperCase: Bool
+        var hasLowerCase: Bool
+        var hasNumber: Bool
+        var hasSpecialCase: Bool
+        
+        var isValid: Bool {
+            hasMinLength && hasUpperCase && hasLowerCase && hasNumber && hasSpecialCase
         }
+    }
+    // Form validation state
+      var isFormValid: Bool {
+          emailError == nil && passwordError == nil && !email.isEmpty && !password.isEmpty
+      }
+    
+    // validation for password
+    private func checkPasswordRequirments() -> PasswordRequirments {
+        return PasswordRequirments(hasMinLength: password.count >= 6,
+                                   hasUpperCase: password.rangeOfCharacter(from: .uppercaseLetters) != nil,
+                                   hasLowerCase: password.rangeOfCharacter(from: .lowercaseLetters) != nil,
+                                   hasNumber: password.rangeOfCharacter(from: .decimalDigits) != nil,
+                                   hasSpecialCase: password.rangeOfCharacter(from: .symbols) != nil
+        )
+    }
+    //the message validation for password
+    func validatePassword() {
+        let requirments = checkPasswordRequirments()
+        
+        if requirments.isValid {
+            passwordError = nil
+        } else {
+            var errors = [String]()
+            if !requirments.hasMinLength {errors.append("at least 6 characters")}
+            if !requirments.hasUpperCase {errors.append("an uppercase letter")}
+            if !requirments.hasLowerCase {errors.append("an lowercase letter")}
+            if !requirments.hasNumber {errors.append("a number")}
+            if !requirments.hasSpecialCase {errors.append("a special character")}
+            
+            passwordError = "Password must contain:\n" + errors.joined(separator: ", ")
+        }
+    }
+    // validating email using regex
+    func validateEmail() {
+        let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let result = email.range(of: emailPattern, options: .regularExpression)
+        emailError = (result == nil || email.isEmpty) ? "Please enter a valid email" : nil
+    }
+    // create a user if it is successful
+    func signUp() {
         Task {
             do {
                 let returnUserData = try await FirebaseAuthService().createUser(email: email, password: password)
